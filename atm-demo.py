4ca6a8652169bc49662d3492f9eeb1db6b25f661
J0hnMilt0n <mjohnmilton41@outlook.com>
2025-01-31 17:51:14 +0530

    updated 3, 4 options

--- atm-demo.py
+++ atm-demo.py
@@ -1,10 +1,11 @@
-Balance = 10000
-user_pin = None # 1234
+from datetime import datetime
+Balance = 10000 #10900 #5900
+user_pin = None # 1234 -> 1432
 inserted = False # True # False
 blocked = False 
 attempt = 0 #1 #2 #3
-print("Welcome to SBI")
-user_pin = int(input("Set the PIN : "))
+transactions = [] # [1000, -100, -5000 ]
+print("Welcome to SBI") #john@pentagonspace.in
 
 while True:
     if inserted == False:
@@ -12,6 +13,8 @@ while True:
         x = int(input("1 - Inserted or 2 - for not Inserted"))
         if x == 1:
            inserted = True
+    if not user_pin:
+        user_pin = int(input("Set the PIN : "))
     if blocked == True:
         print("Your Card has been blocked, contact your Branch manager to unblock")
         break
@@ -32,6 +35,7 @@ while True:
             if amount % 100 == 0:
                 print("Cash has been accepted")
                 Balance = Balance + amount
+                transactions.append(amount)
                 print("Available Balance is ", Balance)
             else:
                 print("Invalid Cash or feed only multiples of 100")
@@ -41,11 +45,44 @@ while True:
                 if amount % 100 == 0:
                     print("Take the Cash")
                     Balance = Balance - amount
+                    transactions.append(-amount)
                     print("Available Balance is ", Balance)
                 else:
                     print("Invalid Cash or feed only multiples of 100")
             else:
                 print("Insufficient Balance")
+
+        if option == 3:
+
+            datim = datetime.now()
+
+            date = datim.strftime("%d-%m-%Y")
+
+            time = datim.strftime("%I:%M %p")
+            print(
+
+                f"""
+                                State Bank of India
+                Date: {date}                                       Time: {time}
+
+                Last Transactions: """)
+            
+            for t in transactions:
+                print(f"""                                       {t}""")
+                                        
+
+            print(f"""Available Balance:      {Balance}
+
+                """
+            )
+        if option == 4:
+            old_pin = int(input("Enter Old PIN : "))
+            if old_pin == user_pin:
+                user_pin = int(input("Enter New PIN : "))
+            else:
+                print("Invalid PIN, Try again")
+                continue
+
 
     else:
         attempt += 1
@@ -59,9 +96,10 @@ while True:
     if cont == '1':
         continue
     elif cont=='':
+        print("Thank you for visiting SBI ....")
         inserted = False 
 
-print("Thank you for visiting SBI ....")
+
 
 
 
